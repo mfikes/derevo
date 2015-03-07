e7e3c366166e550e2a1101b510d781d2ebd0796b
Mike Fikes <mike@fikesfarm.com>
2015-03-07 12:35:36 -0500

    Add initial project structure

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject derevo "0.1.0-SNAPSHOT"
+  :description "Create :foreign-libs graph from JS files."
+  :url "https://github.com/mfikes/derevo"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
